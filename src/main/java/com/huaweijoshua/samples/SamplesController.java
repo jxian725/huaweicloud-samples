743b493df09f4dc4a8f34ba07ece0bee7bcc106d
joshuaooi <ooi.jing.xian@huawei.com>
2023-08-07 18:11:27 +0800

    movec hwc auth to new class for caching

--- src/main/java/com/huaweijoshua/samples/SamplesController.java
+++ src/main/java/com/huaweijoshua/samples/SamplesController.java
@@ -2,6 +2,7 @@ package com.huaweijoshua.samples;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.*;
@@ -16,6 +17,9 @@ import java.util.Objects;
 public class SamplesController {
     Logger logger = LoggerFactory.getLogger(SamplesController.class);
     private final ApplicationConfigProperties applicationConfig;
+
+    @Autowired
+    private HWCAuthenticationService HWCAuth;
 
     public SamplesController(ApplicationConfigProperties applicationConfig) {
         this.applicationConfig = applicationConfig;
@@ -36,7 +40,7 @@ public class SamplesController {
     @ResponseBody
     public String HW_OCR_API(@RequestBody String enc_base64) {
         String base64 = enc_base64.replaceAll("%2F", "/");
-        String token = getToken();
+        String token = HWCAuth.getToken(applicationConfig.authName(),applicationConfig.authPassword(),applicationConfig.authDomain());
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -45,17 +49,5 @@ public class SamplesController {
         HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
         ResponseEntity<String> response = restTemplate.postForEntity("https://ocr.ap-southeast-2.myhuaweicloud.com/v2/" + applicationConfig.projectID() + "/ocr/web-image", entity,String.class);
         return response.getBody();
-    }
-
-    @Cacheable("xst")
-    public String getToken(){
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String requestJson = "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\""+ applicationConfig.authName() + "\",\"password\":\"" + applicationConfig.authPassword() + "\",\"domain\":{\"name\":\"" + applicationConfig.authDomain() + "\"}}}}, \"scope\":{\"project\":{\"name\":\"ap-southeast-2\"}}}}";
-        HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity("https://iam.ap-southeast-2.myhuaweicloud.com/v3/auth/tokens", entity,String.class);
-        HttpHeaders resHeaders = response.getHeaders();
-        return Objects.requireNonNull(resHeaders.get("X-Subject-Token")).toString().replaceAll("\\[", "").replaceAll("\\]","");
     }
 }
