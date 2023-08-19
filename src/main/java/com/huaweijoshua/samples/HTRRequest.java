f6f2a689d66ffe1eb96429e7c24eb187b19764a5
joshuaooi <ooi.jing.xian@huawei.com>
2023-08-19 12:06:31 +0800

    update for HTR (in progress)

---
+++ src/main/java/com/huaweijoshua/samples/HTRRequest.java
@@ -0,0 +1,23 @@
+package com.huaweijoshua.samples;
+
+public class HTRRequest {
+    private String strokes;
+    private String ip;
+    public String getStrokes() {
+        return strokes;
+    }
+
+    // Setter
+    public void setStrokes(String newStroke) {
+        this.strokes = newStroke;
+    }
+
+    public String getip() {
+        return ip;
+    }
+
+    // Setter
+    public void setip(String newip) {
+        this.ip = newip;
+    }
+}
