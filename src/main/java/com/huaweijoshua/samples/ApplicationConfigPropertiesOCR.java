b4e513e792dae9603ad2d484edde53eac65ec99f
JXian <66273113+jxian725@users.noreply.github.com>
2023-08-20 19:23:38 +0800

    added ak/sk for partner

--- src/main/java/com/huaweijoshua/samples/ApplicationConfigPropertiesOCR.java
+++ src/main/java/com/huaweijoshua/samples/ApplicationConfigPropertiesOCR.java
@@ -3,5 +3,5 @@ package com.huaweijoshua.samples;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("ocrwi")
-public record ApplicationConfigProperties(String authDomain, String authName, String authPassword, String projectID) {
+public record ApplicationConfigPropertiesOCR(String authDomain, String authName, String authPassword, String projectID) {
 }
